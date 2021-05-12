e004296778e8f3828922c3c6f465d66cacc27a07
nnzzll <nnzzll@zju.edu.cn>
2021-05-12 09:28:54 +0800

    子数组异或查询

--- 位运算/1310.cpp
+++ 位运算/1310.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int>xor;
-        xor.push_back(0);
+    vector<int> xor_arrQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int>xor_arr;
+        xor_arr.push_back(0);
         for(auto a:arr)
-            xor.push_back(xor.back()^a);
+            xor_arr.push_back(xor_arr.back()^a);
         vector<int>res;
         for(auto &q:queries)
-            res.push_back(xor[q[0]]^xor[q[1]+1]);
+            res.push_back(xor_arr[q[0]]^xor_arr[q[1]+1]);
         return res;
     }
 };

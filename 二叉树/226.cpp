3e3bd5709d2398c404742203ef51add3e560d724
nnzzll <nnzzll@zju.edu.cn>
2021-05-27 09:32:54 +0800

    翻转二叉树

--- 二叉树/226.cpp
+++ 二叉树/226.cpp
@@ -10,6 +10,13 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-
+        if(root){
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
     }
 };

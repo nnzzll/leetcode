fb1d9b595161ef504e7f12833271d7879c770d9e
nnzzll <nnzzll@zju.edu.cn>
2021-06-03 11:47:01 +0800

    和为K的子数组

--- 前缀和/560.cpp
+++ 前缀和/560.cpp
@@ -1,8 +1,22 @@
-#include<vector>
+#include <vector>
+#include <unordered_map>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int prefix = 0;
+        int count = 0;
+        unordered_map<int, int> dic;
+        dic[0] = 1;
+        for (auto num : nums)
+        {
+            prefix += num;
+            if (dic.count(prefix - k))
+                count += dic[prefix-k];
+            dic[prefix]++;
+        }
+        return count;
     }
 };

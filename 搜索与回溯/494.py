4868054603eb9e8d5ca0a5469cc5c51c20fdd9a5
nnzzll <nnzzll@zju.edu.cn>
2021-06-09 09:59:26 +0800

    记忆化搜索

--- 搜索与回溯/494.py
+++ 搜索与回溯/494.py
@@ -1,22 +1,15 @@
 from typing import List
+from functools import lru_cache
 
 
 class Solution:
-    def __init__(self) -> None:
-        self.count = 0
-
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        self.dfs(nums,0,0,target)
-        return self.count
-
-    def dfs(self,nums: list, idx, Sum: int, target: int):
-        if(idx == len(nums)):
-            if(Sum == target):
-                self.count += 1
-        else:
-            self.dfs(nums, idx+1, Sum+nums[idx], target)
-            self.dfs(nums, idx+1, Sum-nums[idx], target)
-
+        @lru_cache(None)
+        def dfs(idx,t):
+            if idx == len(nums):
+                return 1 if t==0 else 0
+            return dfs(idx+1,t-nums[idx]) + dfs(idx+1,t+nums[idx])
+        return dfs(0,target)
 
 nums = [1, 1, 1, 1, 1]
 target = 3

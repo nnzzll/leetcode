6942268ac2110c63851416a3395984f099cb6db0
nnzzll <3150103454@zju.edu.cn>
2021-06-25 10:13:16 +0800

    按序打印

---
+++ 多线程/1114.py
@@ -0,0 +1,85 @@
+from threading import Lock
+import json
+import threading
+
+
+class Foo:
+    def __init__(self):
+        self.firstdone = Lock()
+        self.seconddone = Lock()
+        self.firstdone.acquire()
+        self.seconddone.acquire()
+        pass
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first".
+        printFirst()
+        self.firstdone.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        with self.firstdone:
+        # printSecond() outputs "second".
+            printSecond()
+            self.seconddone.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+
+        # Wait for the second job to be done.
+        with self.seconddone:
+        # printThird() outputs "third".
+            printThird()
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+# def main():
+#     import sys
+#     import io
+#     def readlines():
+#         for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+#             yield line.strip('\n')
+
+#     lines = readlines()
+#     while True:
+#         try:
+#             line = next(lines)
+#             nums = stringToIntegerList(line);
+
+#             ret = Solution().foobar(nums)
+
+#             out = str(ret);
+#             print(out)
+#         except StopIteration:
+#             break
+class Solution:
+    def __init__(self) -> None:
+        self.foo = Foo()
+        pass
+
+    def foobar(self, nums):
+        pool = []
+        for num in nums:
+            if num==1:
+                pool.append(threading.Thread(target=self.foo.first,args=(print1,)))
+            elif num==2:
+                pool.append(threading.Thread(target=self.foo.second,args=(print2,)))
+            elif num==3:
+                pool.append(threading.Thread(target=self.foo.third,args=(print3,)))
+        for p in pool:
+            p.start()
+        for p in pool:
+            p.join()
+
+def print1():
+    print("First")
+
+def print2():
+    print("Second")
+
+def print3():
+    print("Third")
+
+
+Solution().foobar([1,3,2])

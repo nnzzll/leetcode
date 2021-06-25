cab3be379a537057a75fe463b0a11577bd9d077d
nnzzll <3150103454@zju.edu.cn>
2021-06-25 10:13:39 +0800

    按序打印

--- 多线程/1114.py
+++ 多线程/1114.py
@@ -29,30 +29,6 @@ class Foo:
         # printThird() outputs "third".
             printThird()
 
-
-def stringToIntegerList(input):
-    return json.loads(input)
-
-
-# def main():
-#     import sys
-#     import io
-#     def readlines():
-#         for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
-#             yield line.strip('\n')
-
-#     lines = readlines()
-#     while True:
-#         try:
-#             line = next(lines)
-#             nums = stringToIntegerList(line);
-
-#             ret = Solution().foobar(nums)
-
-#             out = str(ret);
-#             print(out)
-#         except StopIteration:
-#             break
 class Solution:
     def __init__(self) -> None:
         self.foo = Foo()
